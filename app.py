cde3a9acb0f7e3abc9ca5419dc05b53b6b9c2522
Ashraf khan <gdak9121997@gmail.com>
2022-11-30 19:20:14 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.write("""
+# Even - Odd Calculator
+
+This app tell whether given number is odd or not. 
+""")
+
+#Take Input from user
+st.header('User Input')
+
+def user_input():
+    num = st.number_input("Enter an integer",min_value=0,step=1)
+
+    return num
+
+num = user_input()
+
+st.subheader('User Input Number')
+st.write(num)
+
+#prepocessing
+def odd_even(num):
+    if num % 2 ==0:
+        return f"{num} is even number."
+    return f"{num} is odd number."
+
+st.write(odd_even(num))
